fa0760688debe0e673be334d9e41271b5ac14c55
Daniel Hermon <danielhermonsync@gmail.com>
2020-12-31 16:58:59 +0200

    feat(server): added @map-colonies/error-express-handler

--- src/serverBuilder.ts
+++ src/serverBuilder.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { getErrorHandlerMiddleware } from '@map-colonies/error-express-handler';
 import { initAsync as validatorInit } from 'openapi-validator-middleware';
 import { container, inject, injectable } from 'tsyringe';
 import { RequestLogger } from './common/middlewares/RequestLogger';
-import { ErrorHandler } from './common/middlewares/ErrorHandler';
 import { Services } from './common/constants';
 import { IConfig, ILogger } from './common/interfaces';
 import { resourceNameRouterFactory } from './resourceName/routes/resourceNameRouter';
@@ -16,8 +16,7 @@ export class ServerBuilder {
   public constructor(
     @inject(Services.LOGGER) private readonly logger: ILogger,
     @inject(Services.CONFIG) private readonly config: IConfig,
-    private readonly requestLogger: RequestLogger,
-    private readonly errorHandler: ErrorHandler
+    private readonly requestLogger: RequestLogger
   ) {
     this.serverInstance = express();
   }
@@ -37,12 +36,12 @@ export class ServerBuilder {
 
     this.serverInstance.use('/resourceName', resourceNameRouterFactory(container));
     this.serverInstance.use('/', swaggerRouterFactory(container));
+    this.serverInstance.use(getErrorHandlerMiddleware((message) => this.logger.log('error', message)));
   }
 
   private registerMiddleware(): void {
     this.logger.log('debug', 'Registering middlewares');
     this.serverInstance.use(bodyParser.json());
     this.serverInstance.use(this.requestLogger.getLoggerMiddleware());
-    this.serverInstance.use(this.errorHandler.getErrorHandlerMiddleware());
   }
 }
