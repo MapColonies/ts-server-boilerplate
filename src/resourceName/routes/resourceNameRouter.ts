59f6aa5b864ff0b8a4deca77dfe55bbb3ce63db8
syncush <danielhermonsync@gmail.com>
2021-01-01 00:41:34 +0200

    refactor(server): changed openapi validator

--- src/resourceName/routes/resourceNameRouter.ts
+++ src/resourceName/routes/resourceNameRouter.ts
@@ -1,14 +1,13 @@
 import { Router } from 'express';
 import { FactoryFunction } from 'tsyringe';
-import { validate } from 'openapi-validator-middleware';
 import { ResourceNameController } from '../controllers/resourceNameController';
 
 const resourceNameRouterFactory: FactoryFunction<Router> = (dependencyContainer) => {
   const router = Router();
   const controller = dependencyContainer.resolve(ResourceNameController);
 
-  router.get('/', validate, controller.getResource);
-  router.post('/', validate, controller.createResource);
+  router.get('/', controller.getResource);
+  router.post('/', controller.createResource);
 
   return router;
 };
