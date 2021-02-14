c6831185dcdeefd68f1faf4c3cfd3e486ccd0967
galta95 <48960890+galta95@users.noreply.github.com>
2021-02-14 11:29:34 +0200

    chore: added test coverage config (#38)
    * chore: added test coverage config
    
    * test: extended coverage
    
    Co-authored-by: Daniel Hermon <danielhermonsync@gmail.com>

--- tests/configurations/integration/jest.config.js
+++ tests/configurations/integration/jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest',
   },
   coverageReporters: ['text', 'html'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!*/node_modules/', '!/vendor/**', '!*/common/**', '!**/models/**', '!<rootDir>/src/*'],
+  coverageDirectory: '<rootDir>/coverage',
   rootDir: '../../../.',
   testMatch: ['<rootDir>/tests/integration/**/*.spec.ts'],
   setupFiles: ['<rootDir>/tests/configurations/jest.setup.js'],
@@ -18,4 +21,12 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts}', '!**/node_modules/**', '!**/vendor/**'],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
 };
