fc414fc4d5414bb77b305ab7124d42ee34d92bbd
Daniel Hermon <danielhermonsync@gmail.com>
2021-05-12 10:26:46 +0300

    style:  prettier

--- standard-version-updater.js
+++ standard-version-updater.js
@@ -9,10 +9,10 @@ module.exports.readVersion = function (contents) {
     throw e;
   }
   return doc.info.version;
-}
+};
 
 module.exports.writeVersion = function (contents, version) {
   const doc = yaml.load(contents, 'utf8');
   doc.info.version = version;
   return yaml.dump(doc, { indent: 2 });
-}
+};
