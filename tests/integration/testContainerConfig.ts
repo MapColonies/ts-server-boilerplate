f3f70c31c9bd1081b08a9dbfc7dfbd5bcb2f339f
Daniel Hermon <danielhermonsync@gmail.com>
2021-07-11 13:42:09 +0300

    fix: tests

--- tests/integration/testContainerConfig.ts
+++ tests/integration/testContainerConfig.ts
@@ -1,7 +1,7 @@
 import { container } from 'tsyringe';
 import config from 'config';
-import { NoopTracer } from '@opentelemetry/api';
-import { Tracing, Metrics } from '@map-colonies/telemetry';
+import { trace } from '@opentelemetry/api';
+import { Metrics } from '@map-colonies/telemetry';
 import jsLogger from '@map-colonies/js-logger';
 import { Services } from '../../src/common/constants';
 
@@ -9,7 +9,9 @@ function registerTestValues(): void {
   container.register(Services.CONFIG, { useValue: config });
   container.register(Services.LOGGER, { useValue: jsLogger({ enabled: false }) });
 
-  container.register(Services.TRACER, { useValue: NoopTracer });
+  // if sdk is not initialized then getTracer returns a NoopTracer
+  const testTracer = trace.getTracer('testTracer');
+  container.register(Services.TRACER, { useValue: testTracer });
 
   const metrics = new Metrics('app_meter');
   const meter = metrics.start();
