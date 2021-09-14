b14d6f3ed540222be5607a7f519aefadac736fa6
Daniel Hermon <danielhermonsync@gmail.com>
2021-09-14 14:29:19 +0300

    style: prettier

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,4 +1,3 @@
-
 import { readPackageJsonSync } from '@map-colonies/read-pkg';
 
 export const SERVICE_NAME = readPackageJsonSync().name ?? 'unknown_service';
@@ -8,10 +7,10 @@ export const IGNORED_OUTGOING_TRACE_ROUTES = [/^.*\/v1\/metrics.*$/];
 export const IGNORED_INCOMING_TRACE_ROUTES = [/^.*\/docs.*$/];
 
 /* eslint-disable @typescript-eslint/naming-convention */
-export const SERVICES : Record<string, symbol> =  {
+export const SERVICES: Record<string, symbol> = {
   LOGGER: Symbol('Logger'),
   CONFIG: Symbol('Config'),
   TRACER: Symbol('Tracer'),
-  METER: Symbol('Meter')
+  METER: Symbol('Meter'),
 };
 /* eslint-enable @typescript-eslint/naming-convention */
