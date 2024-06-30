b55bc11fcc50966fb7fb3ada39e626a0f244754b
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2024-06-30 17:07:48 +0300

    refactor: integrate new package

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,27 @@ import { createServer } from 'http';
 import { createTerminus } from '@godaddy/terminus';
 import { Logger } from '@map-colonies/js-logger';
 import { container } from 'tsyringe';
-import config from 'config';
-import { DEFAULT_SERVER_PORT, SERVICES } from './common/constants';
+// import config from 'config';
+import { ConfigInstance } from '@map-colonies/config';
+import { commonBoilerplateV3Type } from '@map-colonies/schemas';
 
+import { SERVICES } from './common/constants';
 import { getApp } from './app';
 
-const port: number = config.get<number>('server.port') || DEFAULT_SERVER_PORT;
+void getApp()
+  .then(([app]) => {
+    const logger = container.resolve<Logger>(SERVICES.LOGGER);
+    const config = container.resolve<ConfigInstance<commonBoilerplateV3Type>>(SERVICES.CONFIG);
+    const port = config.get('server.port');
+    const stubHealthCheck = async (): Promise<void> => Promise.resolve();
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const server = createTerminus(createServer(app), { healthChecks: { '/liveness': stubHealthCheck, onSignal: container.resolve('onSignal') } });
 
-const app = getApp();
-
-const logger = container.resolve<Logger>(SERVICES.LOGGER);
-const stubHealthCheck = async (): Promise<void> => Promise.resolve();
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const server = createTerminus(createServer(app), { healthChecks: { '/liveness': stubHealthCheck, onSignal: container.resolve('onSignal') } });
-
-server.listen(port, () => {
-  logger.info(`app started on port ${port}`);
-});
+    server.listen(port, () => {
+      logger.info(`app started on port ${port}`);
+    });
+  })
+  .catch((error: Error) => {
+    console.error('😢 - failed initializing the server');
+    console.error(error);
+  });
