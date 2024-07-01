09446c87f1fe67a4d037013219ff2774efcb7fb5
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2024-07-01 15:59:46 +0300

    chore: more work on integration

---
+++ src/common/config.ts
@@ -0,0 +1,25 @@
+import { ConfigInstance, config } from "@map-colonies/config";
+import { commonBoilerplateV4, commonBoilerplateV4Type } from "@map-colonies/schemas";
+
+type ConfigType = ConfigInstance<commonBoilerplateV4Type>
+
+let configInstance: ConfigType | undefined;
+
+async function initConfig(): Promise<void> {
+  configInstance = await config({
+    configName: 'boilerplate',
+    configServerUrl: 'http://localhost:8080',
+    schema: commonBoilerplateV4,
+    version: 'latest',
+  });
+}
+
+function getConfig(): ConfigType {
+  if (!configInstance) {
+    throw new Error('config not initialized');
+  }
+  return configInstance;
+}
+
+
+export { getConfig, initConfig, ConfigType };
