016ffa4a615e24a12567606cbe1669b8c1096e47
Roee Shachar <shacharroy14@gmail.com>
2024-11-12 16:17:47 +0200

    fix: upgrade commitlint versions and configurations (#320)
    * feat: upgrade commitlint versions and configurations
    
    * refacor: readme
    
    * fix: test failed because of type mismatch
    
    * chore: move husky from devdepenedncy to depenedncy
    
    ---------
    
    Co-authored-by: Roee Shachar <shacharroy142gmail.com>

--- tests/integration/docs/helpers/docsRequestSender.ts
+++ tests/integration/docs/helpers/docsRequestSender.ts
@@ -1,7 +1,8 @@
+import type { Application } from 'express';
 import * as supertest from 'supertest';
 
 export class DocsRequestSender {
-  public constructor(private readonly app: Express.Application) {}
+  public constructor(private readonly app: Application) {}
 
   public async getDocs(): Promise<supertest.Response> {
     return supertest.agent(this.app).get('/docs/api/');
