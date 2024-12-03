74cb6633ed2f10d784767a3db12357e5124267b7
vitaligi <54726763+vitaligi@users.noreply.github.com>
2024-12-03 13:25:14 +0200

    fix: terminus options

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ void getApp()
     const port = config.get('server.port');
     const stubHealthCheck = async (): Promise<void> => Promise.resolve();
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    const server = createTerminus(createServer(app), { healthChecks: { '/liveness': stubHealthCheck, onSignal: container.resolve('onSignal') } });
+    const server = createTerminus(createServer(app), { healthChecks: { '/liveness': stubHealthCheck }, onSignal: container.resolve('onSignal') });
 
     server.listen(port, () => {
       logger.info(`app started on port ${port}`);
