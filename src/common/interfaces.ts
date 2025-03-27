cc23f06fedc9e5bb54df02604f78338f2a99c7d4
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2025-03-27 10:33:23 +0200

    fix: removed unused interface as its imported from the schemas package

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -2,10 +2,3 @@ export interface IConfig {
   get: <T>(setting: string) => T;
   has: (setting: string) => boolean;
 }
-
-export interface OpenApiConfig {
-  filePath: string;
-  basePath: string;
-  jsonPath: string;
-  uiPath: string;
-}
